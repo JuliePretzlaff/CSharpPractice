ce262e9c69a78d841394ab2f6ac3dec1e321464b
Julie <Juliapretzlaff@gmail.com>
2018-05-30 16:59:24 -0700

    completed problem #1

--- cSharpPractice/Program.cs
+++ cSharpPractice/Program.cs
@@ -6,7 +6,33 @@ namespace cSharpPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] stockPricesYesterday = { 10, 7, 5, 8, 11, 9 };
+
+            // Returns 6 (buying for $5 and selling for $11)
+            
+            Console.WriteLine(GetMaxProfit(stockPricesYesterday));
+
+        }
+
+        static int GetMaxProfit(int[] priceArray)
+        {
+            int lowest = priceArray[0];
+            int highest = priceArray[0];
+
+            for (int i =0; i < priceArray.Length; i++)
+            {
+                if (priceArray[i] < lowest)
+                {
+                    lowest = priceArray[i];
+                }
+
+                if(priceArray[i] > highest)
+                {
+                    highest = priceArray[i];
+                }
+            }
+
+            return (highest - lowest);
         }
     }
 }
