8139758f014cf798e2ed103e4ab5d54003c05ecd
Julie <Juliapretzlaff@gmail.com>
2018-05-30 17:19:45 -0700

    fixed problem 1.  still working on writing it better

--- cSharpPractice/Program.cs
+++ cSharpPractice/Program.cs
@@ -6,32 +6,36 @@ namespace cSharpPractice
     {
         static void Main(string[] args)
         {
-            int[] stockPricesYesterday = { 10, 7, 5, 8, 11, 9 };
+            int[] stockPricesYesterday = { 13, 7, 5, 8, 11, 9 };
 
             // Returns 6 (buying for $5 and selling for $11)
-            
-            Console.WriteLine(GetMaxProfit(stockPricesYesterday));
+
+            Console.WriteLine("The max profit possible is: " + GetMaxProfit(stockPricesYesterday));
 
         }
 
         static int GetMaxProfit(int[] priceArray)
         {
             int lowest = priceArray[0];
-            int highest = priceArray[0];
+            int highest = 0;
+            int indexOfLowest=0;
 
-            for (int i =0; i < priceArray.Length; i++)
+            for (int i = 0; i < priceArray.Length; i++)
             {
                 if (priceArray[i] < lowest)
                 {
                     lowest = priceArray[i];
-                }
-
-                if(priceArray[i] > highest)
-                {
-                    highest = priceArray[i];
+                    indexOfLowest = i;
                 }
             }
-
+            Console.WriteLine(indexOfLowest);
+            for (int k = (indexOfLowest); k < priceArray.Length; k++)
+            {
+                if (priceArray[k] > highest)
+                {
+                    highest = priceArray[k];
+                }
+            }
             return (highest - lowest);
         }
     }
